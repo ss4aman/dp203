a6b5e215acd62e535e0c916f9e98b82294c4316c
Aman Puri <puriaman@rocketmail.com>
2025-01-19 13:43:01 -0300

    Changed upto 19

--- Allfiles/labs/18/setup.ps1
+++ Allfiles/labs/18/setup.ps1
@@ -79,32 +79,32 @@ foreach ($provider in $provider_list){
 # Generate unique random suffix
 [string]$suffix =  -join ((48..57) + (97..122) | Get-Random -Count 7 | % {[char]$_})
 Write-Host "Your randomly-generated suffix for Azure resources is $suffix"
-$resourceGroupName = "dp203-$suffix"
+$resourceGroupName = "<rgname>"
 
 # Choose a random region
-Write-Host "Finding an available region. This may take several minutes...";
-$delay = 0, 30, 60, 90, 120 | Get-Random
-Start-Sleep -Seconds $delay # random delay to stagger requests from multi-student classes
-$preferred_list = "australiaeast","centralus","southcentralus","eastus2","northeurope","southeastasia","uksouth","westeurope","westus","westus2"
-$locations = Get-AzLocation | Where-Object {
-    $_.Providers -contains "Microsoft.Synapse" -and
-    $_.Providers -contains "Microsoft.Sql" -and
-    $_.Providers -contains "Microsoft.Storage" -and
-    $_.Providers -contains "Microsoft.Compute" -and
-    $_.Providers -contains "Microsoft.EventHub" -and
-    $_.Providers -contains "Microsoft.StreamAnalytics" -and
-    $_.Location -in $preferred_list
-}
-$max_index = $locations.Count - 1
-# Start with preferred region if specified, otherwise choose one at random
-if ($args.count -gt 0 -And $args[0] -in $locations.Location)
-{
-    $Region = $args[0]
-}
-else {
-    $rand = (0..$max_index) | Get-Random
-    $Region = $locations.Get($rand).Location
-}
+# Write-Host "Finding an available region. This may take several minutes...";
+# $delay = 0, 30, 60, 90, 120 | Get-Random
+# Start-Sleep -Seconds $delay # random delay to stagger requests from multi-student classes
+# $preferred_list = "australiaeast","centralus","southcentralus","eastus2","northeurope","southeastasia","uksouth","westeurope","westus","westus2"
+# $locations = Get-AzLocation | Where-Object {
+#     $_.Providers -contains "Microsoft.Synapse" -and
+#     $_.Providers -contains "Microsoft.Sql" -and
+#     $_.Providers -contains "Microsoft.Storage" -and
+#     $_.Providers -contains "Microsoft.Compute" -and
+#     $_.Providers -contains "Microsoft.EventHub" -and
+#     $_.Providers -contains "Microsoft.StreamAnalytics" -and
+#     $_.Location -in $preferred_list
+# }
+# $max_index = $locations.Count - 1
+# # Start with preferred region if specified, otherwise choose one at random
+# if ($args.count -gt 0 -And $args[0] -in $locations.Location)
+# {
+$Region = "<regionname>"
+# }
+# else {
+#     $rand = (0..$max_index) | Get-Random
+#     $Region = $locations.Get($rand).Location
+# }
 
 # Test for subscription Azure SQL capacity constraints in randomly selected regions
 # (for some subsription types, quotas are adjusted dynamically based on capacity)
--- Allfiles/labs/19/setup.ps1
+++ Allfiles/labs/19/setup.ps1
@@ -50,23 +50,23 @@ foreach ($provider in $provider_list){
 # Generate unique random suffix
 [string]$suffix =  -join ((48..57) + (97..122) | Get-Random -Count 7 | % {[char]$_})
 Write-Host "Your randomly-generated suffix for Azure resources is $suffix"
-$resourceGroupName = "dp203-$suffix"
+$resourceGroupName = "<rgname>"
 
 # Choose a random region
-$locations = Get-AzLocation | Where-Object {
-    $_.Providers -contains "Microsoft.EventHub" -and
-    $_.Providers -contains "Microsoft.StreamAnalytics"
-}
-$max_index = $locations.Count - 1
-# Start with preferred region if specified, otherwise choose one at random
-if ($args.count -gt 0 -And $args[0] -in $locations.Location)
-{
-    $Region = $args[0]
-}
-else {
-    $rand = (0..$max_index) | Get-Random
-    $Region = $locations.Get($rand).Location
-}
+# $locations = Get-AzLocation | Where-Object {
+#     $_.Providers -contains "Microsoft.EventHub" -and
+#     $_.Providers -contains "Microsoft.StreamAnalytics"
+# }
+# $max_index = $locations.Count - 1
+# # Start with preferred region if specified, otherwise choose one at random
+# if ($args.count -gt 0 -And $args[0] -in $locations.Location)
+# {
+$Region = "<regionname>"
+# }
+# else {
+#     $rand = (0..$max_index) | Get-Random
+#     $Region = $locations.Get($rand).Location
+# }
 
 Write-Host "Creating $resourceGroupName resource group in $Region ..."
 New-AzResourceGroup -Name $resourceGroupName -Location $Region | Out-Null
